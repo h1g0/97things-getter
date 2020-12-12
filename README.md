52e3932418eb8d396cb654e94d52b4a8ea598987
hi_go <me@clutte.red>
2020-12-13 01:46:58 +0900

    Write `README.md`.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # 97things-getter
-getter for https://xn--97-273ae6a4irb6e2hsoiozc2g4b8082p.com/
+
+[プログラマが知るべき97のこと](https://xn--97-273ae6a4irb6e2hsoiozc2g4b8082p.com/)というサイトから、掲載されている107個のエッセイのタイトルとURLを取得してファイルに出力します。
+
+また、bit.lyを使用し、URLを短縮して出力することも可能です。
+
+## 動作に必要な環境
+
+- Python3.6以上
+- requests-html（`pip3 install requests_html`でインストール）
+
+## 使用法
+
+```bash
+$ python main.py
+```
+
+を実行すると、`result.txt`に結果が出力されます。
+
+## URLを短縮する場合
+
+1. bit.lyでアカウントを作り、access tokenを取得しておく
+2. `main.py`と同じディレクトリに`access_token.txt`を作成し、中に取得したaccess_tokenを記述しておく
+3. `main.py`の`make_url_short = False`を`True`に変える
+4. `main.py`を実行
--- main.py
+++ main.py
@@ -6,26 +6,28 @@ from requests_html import HTMLSession
 from shorten_url import load_access_token_from_file, shorten_url
 
 base_url = 'https://xn--97-273ae6a4irb6e2hsoiozc2g4b8082p.com/'
-make_url_short = False
+make_url_short = False  # URLを短縮するか否かのフラグ
+
 
 def main():
     requests = HTMLSession()
     res = requests.get(base_url)
-    links = res.html.find('ol',first = True).find('a')
+    links = res.html.find('ol', first=True).find('a')
     token = ''
     if make_url_short:
         token = load_access_token_from_file('access_token.txt')
-    with open('result.txt',mode='w',encoding='utf8') as f:
+    with open('result.txt', mode='w', encoding='utf8') as f:
         for link in links:
             title = link.text
-            url = urllib.parse.urljoin(base_url,link.attrs['href'])
+            url = urllib.parse.urljoin(base_url, link.attrs['href'])
 
             if make_url_short:
-                url = shorten_url(token,url)
+                url = shorten_url(token, url)
                 time.sleep(1)
-            print('{} - {}'.format(title,url))
-            f.writelines('{} - {}\n'.format(title,url))
-    
+            print(f'{title} - {url}')
+            f.writelines(f'{title} - {url}\n')
+
+
 # エントリポイント
 if __name__ == '__main__':
     main()
