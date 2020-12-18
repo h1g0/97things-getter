416f6a3d27d2acd2c3a7531438e1e3d0634c1c37
hi_go <me@clutte.red>
2020-12-19 00:45:48 +0900

    Change `base_url`.

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from requests_html import HTMLSession
 
 from shorten_url import load_access_token_from_file, shorten_url
 
-base_url = 'https://xn--97-273ae6a4irb6e2hsoiozc2g4b8082p.com/'
+#base_url = 'https://xn--97-273ae6a4irb6e2hsoiozc2g4b8082p.com/'
+base_url = 'https://ja.wikisource.org/wiki/%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%81%8C%E7%9F%A5%E3%82%8B%E3%81%B9%E3%81%8D97%E3%81%AE%E3%81%93%E3%81%A8'
 make_url_short = False  # URLを短縮するか否かのフラグ
 
 
--- shorten_url.py
+++ shorten_url.py
@@ -2,21 +2,23 @@ from requests_html import HTMLSession
 import json
 
 
-def load_access_token_from_file(file_name='access_token.txt'):
+def load_access_token_from_file(file_name: str = 'access_token.txt') -> str:
     with open(file_name, mode='r') as f:
-        result = f.readline().replace('\n','')
-        print(result)
+        result = f.readline().replace('\n', '')
         return result
 
-def shorten_url(access_token, url):
+
+def shorten_url(access_token: str, url: str) -> str:
     api_url = 'https://api-ssl.bitly.com/v3/shorten'
     param = {
-            "access_token": access_token,
-            "longUrl": url
-            }
+        "access_token": access_token,
+        "longUrl": url
+    }
     req = HTMLSession()
-    res = req.request('GET',api_url, params=param)
+    res = req.request('GET', api_url, params=param)
     return res.json()['data']['url']
+
+
 '''
 def shorten_url(access_token, url):
     api_url = 'https://api-ssl.bitly.com/v4/shorten'
@@ -31,4 +33,4 @@ def shorten_url(access_token, url):
 '''
 if __name__ == "__main__":
     token = load_access_token_from_file('access_token.txt')
-    print(shorten_url(token,'https://google.com/'))
+    print(shorten_url(token, 'https://google.com/'))
